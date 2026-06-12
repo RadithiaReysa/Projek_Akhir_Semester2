576937aabc6dd4c0fdb0ff662a5e94ccf44c579f
Wellin-Rttf <petrasianturi.1210@gmail.com>
2026-06-12 08:49:09 +0700

    Update models.py

--- models.py
+++ models.py
@@ -7,18 +7,3 @@ class Kandidat:
 
     def tampil_ringkas(self):
         return f"{self.nomor}. {self.nama} | Suara: {self.suara}"
-    
-    def tampil_detail(self):
-
-        print("Nomor :", self.nomor)
-        print("Nama :", self.nama)
-        print("Visi Misi :", self.visi_misi)
-        print("Suara :", self.suara)
-
-
-class Pemilih:
-
-    def __init__(self, nim, nama):
-
-        self.nim = nim
-        self.nama = nama
